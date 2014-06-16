e3b8585781aa6d9c571cb08bcedf58f4f0a55645
Michael Chu <CoderMChu@users.noreply.github.com>
2014-06-16 14:19:27 +0100

    Update create_peter_user_with_sudo_access.sh

--- create_peter_user_with_sudo_access.sh
+++ create_peter_user_with_sudo_access.sh
@@ -2,13 +2,13 @@
 
 MY_SHELL=$( which bash )
 
-/usr/sbin/useradd -c 'Peter G. Williams <petergwilliams at hotmail.com>' -s ${MY_SHELL} -m peter
-cat >/etc/sudoers.d/peter-ALL <<EOF
-peter     ALL=(ALL:ALL) NOPASSWD: ALL
+/usr/sbin/useradd -c 'Michael CHu <mch@ihtsdo.org' -s ${MY_SHELL} -m michael
+cat >/etc/sudoers.d/michael-ALL <<EOF
+michael     ALL=(ALL:ALL) NOPASSWD: ALL
 EOF
-chmod 0440 /etc/sudoers.d/peter-ALL
-mkdir ~peter/.ssh
-chmod 700 ~peter/.ssh
-curl https://raw.githubusercontent.com/pgwilliams/ssh_access/master/petergwilliams_rsa.pub >~peter/.ssh/authorized_keys
-chmod 400 ~peter/.ssh/authorized_keys
-chown -R peter ~peter/.ssh
+chmod 0440 /etc/sudoers.d/michael-ALL
+mkdir ~michael/.ssh
+chmod 700 ~michael/.ssh
+curl https://raw.githubusercontent.com/CoderMChu/ssh_access/master/mchu_rsa.pub >~michael/.ssh/authorized_keys
+chmod 400 ~michael/.ssh/authorized_keys
+chown -R michael ~michael/.ssh
